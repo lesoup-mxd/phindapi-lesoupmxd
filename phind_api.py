5b29f53af0ade07f06c640b9636ad2a674dd51cd
lesoup-mxd <le.soup@hotmail.com>
2023-03-22 14:58:37 +0200

    Added a branch for unfinished code (DOESNT WORK)

--- phind_api.py
+++ phind_api.py
@@ -8,6 +8,7 @@ except Exception as exception:
     exception.with_traceback()
 
 
+
 class browser():
     def __init__(self, headless=True, useDetailedAnswer=False, useCreativeAnswer=True):
         options = uc.ChromeOptions()
@@ -16,10 +17,10 @@ class browser():
         self.driver = uc.Chrome(options=options)
         try:
             self.driver.get('https://staging.phind.com/')
-            self.driver.execute_script("localStorage.setItem('useDetailedAnswer', "+str(useDetailedAnswer).lower()+")")
-            self.driver.execute_script("localStorage.setItem('useCreativeAnswer', "+str(useCreativeAnswer).lower()+")")
+            self.driver.execute_script("localStorage.setItem('useDetailedAnswer', self.useDetailedAnswer)")
+            self.driver.execute_script("localStorage.setItem('useCreativeAnswer', self.useCreativeAnswer)")
         except Exception as exception:
-            print(exception.with_traceback())
+            exception.with_traceback()
     
     def search(self, query='',timeout=30):
         self.driver.get('https://staging.phind.com/search?q='+query)
@@ -30,16 +31,39 @@ class browser():
             return search_results
         except:
             print('Search Timeout error. Time exceeded ', timeout, ' seconds.')
+    def search_stream(self, query='', timeout=10, max_elements=20,retries_max=10):
+        self.driver.get('https://staging.phind.com/search?q='+query)
+        self.wait = WebDriverWait(self.driver, timeout)
+        retries=0
+        for i in range(max_elements):
+            print('Searching for element ', i)
+            while retries<retries_max:
+                try:
+                    self.wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div/div/span/*['+str(i)+']')))
+                    element = self.driver.find_element('xpath', '/html/body/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div/div/span/*['+str(i)+']').text
+                    print(element)
+                    if element!='':
+                        retries=0
+                    break
+                except:
+                    print('Element not found. Retrying in ', timeout, ' seconds.')
+                    retries+=1
+        if retries>=retries_max:
+            print('Search Timeout error. Time exceeded ', timeout, ' seconds.')
+            
+
     def close(self):
         self.driver.quit()
     
 if __name__ == '__main__':
     browser = browser()
     query=input('You are running a python module as a script. Please enter a query:  ')
-    timer=time.time()
+    '''timer=time.time()
     search_results = browser.search(query=query, timeout=60)
     timer=time.time()-timer
     timer=str(timer.__round__(2))+' seconds'
     print(search_results)
     print('Time taken to fetch: ',timer)
+    '''
+    browser.search_stream(query=query, timeout=5, max_elements=20)
     browser.close()
